33e68bbb8ec35a46153e9a2d012af58f8e8b0f62
corsair <tony.corsair.corp@gmail.com>
2024-11-01 23:38:39 +0200

    Add dynamic route, not-found page. Work with db: get all snippets, add snippet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import {db} from "@/db";
+
+
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+
+  return <div>
+    Home Page
+    {snippets.map((snippet) => (<div key={snippet.id}>title: {snippet.title}</div>))}
+  </div>;
 }
---
+++ src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen text-2xl bold">
+      <h1>Sorry, we couldn&apos;t find that particular snippet :(</h1>
+    </div>
+  );
+}
