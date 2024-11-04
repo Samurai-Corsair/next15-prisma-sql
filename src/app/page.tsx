a5fff25b1520e64ca1c5685f50c6c958f5d924c1
corsair <tony.corsair.corp@gmail.com>
2024-11-04 15:44:57 +0200

    Home page update - list of snippets + create new option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-import {db} from "@/db";
-
-
+import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
+  const renderedSnippets = () => {
+    return snippets.map((snippet) => (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+        key={snippet.id}
+      >
+        <div>title: {snippet.title}</div>
+        <div>View</div>
+      </Link>
+    ));
+  };
 
-  return <div>
-    Home Page
-    {snippets.map((snippet) => (<div key={snippet.id}>title: {snippet.title}</div>))}
-  </div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex p-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link className="border p-2 rounded" href={"/snippets/new"}>
+          New
+        </Link>
+      </div>
+      {renderedSnippets()}
+    </div>
+  );
 }
